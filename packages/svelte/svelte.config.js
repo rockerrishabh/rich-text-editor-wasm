b76a4bba85046571801783f5fbe28bde6a82879b
Rishabh Kumar <rishabh.6542@gmail.com>
2025-11-22 14:48:56 +0530

    fix: remove --if-present flag from lint commands to prevent pnpm from passing it to eslint

--- packages/svelte/svelte.config.js
+++ packages/svelte/svelte.config.js
@@ -1,5 +1,8 @@
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
 export default {
-  preprocess: vitePreprocess(),
+  preprocess: vitePreprocess({
+    // Disable TypeScript checking in preprocess - tsc handles it separately
+    typescript: false,
+  }),
 };
